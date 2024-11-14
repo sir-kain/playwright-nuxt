75003f20ce1897f59c117de487f81d05fe6955c9
Waly <ahmadouwalyndiaye@gmail.com>
2024-11-14 11:29:50 +0000

    Nuxt-playwright: bug reproduction

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  modules: ['@nuxt/test-utils/module'],
   devtools: { enabled: true }
 })
---
+++ tests/my-test.spec.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest'
+
+test('my test', () => {
+  expect(2 + 2).toBe(4);
+})
